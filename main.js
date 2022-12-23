92fba40068a7b9aeaa92c09f1f207752be2094be
Mansor Almossa <malmossa@gmail.com>
2022-12-22 20:12:36 -0800

    completed

---
+++ main.js
@@ -0,0 +1,4 @@
+const firstNumber = document.querySelector("#num1")
+const secondNumber = document.querySelector("#num2")
+const button = document.querySelector(".btn")
+const table = document.querySelector(".table-body")

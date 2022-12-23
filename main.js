d72e9b0c0c19816ad46650560a08910a59540263
Mansor Almossa <malmossa@gmail.com>
2022-12-22 22:30:29 -0800

    completed

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ const firstNumber = document.querySelector("#num1")
 const secondNumber = document.querySelector("#num2")
 const button = document.querySelector(".btn")
 const table = document.querySelector(".table-body")
+
+button.addEventListener("click", fizzBuzz)
+
+// Numbers Array
+let numbersArray = []
+
+function fizzBuzz() {
+
+  const input1 = firstNumber.value
+  const input2 = secondNumber.value
+
+  // Generating numbers 1 to 100
+  for(let i = 0; i <= 100; i++) {
+    numbersArray.push(i)
+  }
+}
